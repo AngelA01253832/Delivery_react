84b68da5fd3349ff935751e22202d91541581bfa
Angel Huesca <a01253832@itesm.mx>
2021-11-05 16:48:53 -0700

    Init progress bar order

--- src/Components/Pedido.js
+++ src/Components/Pedido.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Progress from './Stepprogress';
+import "../css/Pedido.css"
 function Pedido(){
-    const [pedidos, setPedidos] = useState(0)
+    const [pedidos, setPedidos] = useState("No hay pedidos existentes")
     return(
     <>
         <h1>Status de mi pedido</h1>
-        <h3>Mis pedidos:{pedidos}</h3>
-        <button onClick={() => setPedidos(pedidos+1)}>Agregar pedido</button>
+        <div className = "StepProgress">
+            <div className = "Progress-container">
+                <Progress></Progress>    
+            </div>
+        </div>
+        <button onClick={() => setPedidos("1 pedido")}>Agregar pedido</button>
     </>
     )
 }
