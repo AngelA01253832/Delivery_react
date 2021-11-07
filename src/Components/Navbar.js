e601508c9d408cf1f8f8b66d6961f60308d54fc7
Angel Bonilla <a01253832@itesm.mx>
2021-11-07 00:31:44 -0700

    Funcional step progress order

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -15,7 +15,39 @@ import {
 function Navbar(){
     return(
         <Router>
-            <nav className="navbar">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/"><h1><span>PIZZA</span> DELI</h1></Link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <Link to="/Tacos">Pizzas</Link>
+                </li>
+                <li class="nav-item">
+                    <Link to="/Hamburguesas">Adicionales</Link>                </li>
+                <li class="nav-item">
+                    <Link to="/Contacto">Contacto</Link>
+                </li>
+                <li class="nav-item">
+                    <Link to="/Pedidos">Status pedido</Link>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Carrito
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                </ul>
+            </div>
+        </nav>
+            {/* <nav className="navbar">
                 <div className="logo">
                     <Link to="/"><h1><span>PIZZA</span> DELI</h1></Link>
                 </div>
@@ -38,7 +70,7 @@ function Navbar(){
                         </li>
                     </div>
                 </ul>
-            </nav>
+            </nav> */}
             <Switch>
                 <Route path="/Tacos">
                     <Taco_menu/>
@@ -50,7 +82,7 @@ function Navbar(){
                     <Contacto/>
                 </Route>
                 <Route path="/Pedidos">
-                    <Pedido/>
+                    <Pedido order = {false}/>
                 </Route>
                 <Route path="/">
                     <Home/>
--- src/Components/Pedido.js
+++ src/Components/Pedido.js
@@ -1,17 +1,41 @@
 import React, { useState } from 'react';
 import Progress from './Stepprogress';
 import "../css/Pedido.css"
-function Pedido(){
-    const [pedidos, setPedidos] = useState("No hay pedidos existentes")
+import imagepedido from '../img/entrega.png'
+// import emptycart from '../img/carritoVacio.png'
+
+function Pedido(props){
+    const [pedidos, setPedidos] = useState(false);
+    const [pedidoscart, setPedidoscart] = useState(0);
+    
+    const orderverification = (e) => setPedidos(e)
+    const confirmorder = () => setPedidoscart(pedidoscart+1)
+    if(pedidos === false){
+        return(
+        <>
+            <h1 className="title">Pedidos existentes: {pedidoscart}</h1>
+            <div className="false-order-container">
+                <button type="button" class="btn btn-success" onClick={ orderverification}>Confirmar orden</button>
+            </div>
+            {/* <div className="img-progress-container">
+                <img src={emptycart}></img>
+            </div> */}
+        </>
+        )
+    }
     return(
     <>
-        <h1>Status de mi pedido</h1>
-        <div className = "StepProgress">
-            <div className = "Progress-container">
-                <Progress></Progress>    
+
+            <h1 className="title">Status de mi pedido</h1>
+            <div className="img-progress-container">
+                <img src={imagepedido}></img>
             </div>
-        </div>
-        <button onClick={() => setPedidos("1 pedido")}>Agregar pedido</button>
+            <div className = "StepProgress">
+                <div className = "Progress-container">
+                    <Progress></Progress>    
+                </div>
+            </div>
+
     </>
     )
 }
